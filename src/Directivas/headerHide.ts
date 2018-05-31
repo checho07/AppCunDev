8facaa01d58a70c29bba270b4b426beb55989056
leidy castiblanco <ledu-19@hotmail.com>
2018-05-31 14:31:47 -0500

    comentar codigo

--- src/Directivas/headerHide.ts
+++ src/Directivas/headerHide.ts
@@ -24,13 +24,22 @@ export class HideHeaderDirective {
     console.log('Hello HideHeaderDirective Directive');
   }
 
-  ngOnInit(){
+  /**
+   * ngOnInit contiene la transaccion y el tamaño del header nativo
+   * 
+   */
+    ngOnInit(){
     this.headerHeight = this.header.clientHeight;
     this.renderer.setElementStyle(this.header, 'webkitTransition', 'top 700ms');
     this.scrollContent = this.element.nativeElement.getElementsByClassName("scroll-content")[0];
     this.renderer.setElementStyle(this.scrollContent, 'webkitTransition', 'margin-top 700ms');
   }
 
+  /**
+   *  setea estilos dependiendo el tamaño  que ocupe el scroll
+   * @param event recibe el tamaño que ocupa el scroll en pantalla
+   */
+  
   onContentScroll(event){
     if(event.scrollTop > 36){
       this.renderer.setElementStyle(this.header, "top", "-258px")
