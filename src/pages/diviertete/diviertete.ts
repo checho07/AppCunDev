e5d583ee9f4c65b765b420ed952e4ec5ec9ab968
leidy castiblanco <ledu-19@hotmail.com>
2018-07-12 11:09:19 -0500

    prueba

--- src/pages/diviertete/diviertete.ts
+++ src/pages/diviertete/diviertete.ts
@@ -78,7 +78,7 @@ export class DiviertetePage {
 
   launchExternalApp(iosSchemaName: string, androidPackageName: string, appUrl: string, httpUrl: string, username: string) {
   
-    let app: string;
+    let app:string;
 
   	if (this.device.platform === 'iOS') {
   		app = iosSchemaName;
@@ -103,7 +103,7 @@ export class DiviertetePage {
   }
 
   openArcunoid(username: string) {
-  	this.launchExternalApp('instagram://', 'com.instagram.android', 'instagram://user?username=', 'https://play.google.com/store/apps/details?id=com.cun.arcunoid', username);
+  	this.launchExternalApp('instagram://', 'com.instagram.android', 'instagram://user?username=', 'https://play.google.com/store/apps', username);
   }
   
   openTwitter(username: string) {
