a1b7c0ea1678bcd95bd4df58e6b046fb0b869e99
leidy castiblanco <ledu-19@hotmail.com>
2018-07-13 16:35:02 -0500

    commit cambios de prueba

---
+++ src/data/data.directorio.ts
@@ -0,0 +1,22 @@
+export const directorio = [
+    {
+      nombre:  "Línea de atención en Bogotá" ,
+      telefono:  "3078180",
+      email:    "contacto@cun.edu.co",
+      iconcall: "call",
+      iconmail: "mail"
+
+    },
+    {
+      nombre: "Línea Gratuita Nacional ",
+      telefono: "018000115411",
+      iconcall: "call"
+    },
+    {
+        nombre:  "Línea de atención en Bogotá" ,
+        telefono:  "3813222",
+        email:    "contacto@cun.edu.co",
+        iconcall: "call",
+        iconmail: "mail"
+    }
+];
